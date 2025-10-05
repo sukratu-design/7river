b55cca74b6ab3ea4afadf36ba17a39a5dec06dc9
Sukratu <100075247+sukratu-design@users.noreply.github.com>
2025-10-05 12:20:54 +0530

    Updated HeatmapPage.tsx

--- src/components/HeatmapPage.tsx
+++ src/components/HeatmapPage.tsx
@@ -1,38 +1,41 @@
-@@ .. @@
- import { FeatherMoon } from "@subframe/core";
- import { FeatherSearch } from "@subframe/core";
+import { FeatherMoon } from "@subframe/core";
+import { FeatherSearch } from "@subframe/core";
 
--function HeatmapPage() {
-+interface HeatmapPageProps {
-+  onNavigateToHome?: () => void;
-+  onNavigateToSearch?: () => void;
-+  onNavigateToAlerts?: () => void;
-+  onNavigateToHeatmap?: () => void;
-+  currentPage?: 'home' | 'search' | 'alerts' | 'heatmap';
-+}
-+
-+function HeatmapPage({ onNavigateToHome, onNavigateToSearch, onNavigateToAlerts, onNavigateToHeatmap, currentPage }: HeatmapPageProps) {
-   return (
--    <DefaultPageLayout>
-+    <DefaultPageLayout 
-+      onNavigateToHome={onNavigateToHome}
-+      onNavigateToSearch={onNavigateToSearch}
-+      onNavigateToAlerts={onNavigateToAlerts}
-+      onNavigateToHeatmap={onNavigateToHeatmap}
-+      currentPage={currentPage}
-+    >
-       <div className="flex w-full flex-col items-start gap-6 bg-neutral-50 px-6 py-6">
-         <div className="flex w-full items-center gap-4">
-           <TextField
-             variant="filled"
-             label=""
-             helpText=""
-             icon={<FeatherSearch />}
-+            onClick={onNavigateToSearch}
-           >
-             <TextField.Input
-               placeholder="Search"
-               value=""
-               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
-+              onFocus={onNavigateToSearch}
-             />
+interface HeatmapPageProps {
+  onNavigateToHome?: () => void;
+  onNavigateToSearch?: () => void;
+  onNavigateToAlerts?: () => void;
+  onNavigateToHeatmap?: () => void;
+  currentPage?: 'home' | 'search' | 'alerts' | 'heatmap';
+}
+
+function HeatmapPage({ onNavigateToHome, onNavigateToSearch, onNavigateToAlerts, onNavigateToHeatmap, currentPage }: HeatmapPageProps) {
+  return (
+    <DefaultPageLayout 
+      onNavigateToHome={onNavigateToHome}
+      onNavigateToSearch={onNavigateToSearch}
+      onNavigateToAlerts={onNavigateToAlerts}
+      onNavigateToHeatmap={onNavigateToHeatmap}
+      currentPage={currentPage}
+    >
+      <div className="flex w-full flex-col items-start gap-6 bg-neutral-50 px-6 py-6">
+        <div className="flex w-full items-center gap-4">
+          <TextField
+            variant="filled"
+            label=""
+            helpText=""
+            icon={<FeatherSearch />}
+            onClick={onNavigateToSearch}
+          >
+            <TextField.Input
+              placeholder="Search"
+              value=""
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {}}
+              onFocus={onNavigateToSearch}
+            />
+          </TextField>
+        </div>
+      </div>
+    </DefaultPageLayout>
+  );
+}
